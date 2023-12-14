6ef90f970233c6f1c2b6401ae51373aecada8365
Anshpreet123 <108647469+Anshpreet123@users.noreply.github.com>
2023-12-14 09:38:37 +0000

    variable done

--- .devcontainer/basics.js
+++ .devcontainer/basics.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+const accountId = 144553
+let accountEmail = "ansh@gmail.com";
+let accountState ;
+// dont use var keywords 
+// javascript didnot used var 
+// because of 
+
+console.log(accountId)
+console.table([accountId , accountEmail  , accountState ])
